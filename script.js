e4015c65286cd256deccdd555ef5c3ac71567b43
brmedeiros <br.medeiros@gmail.com>
2017-09-20 22:17:40 -0300

    improves readability of the code

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 $(function() {
+
     $("#create-new-game-btn").click(function() {
 	$("#root-home").load("waiting-room.html", function() {
-	    $.getScript("/script.js");
+	    //$.getScript("/script.js");
 	    $.getJSON( "/code-gen/", function( data ) {
 		$("#code").text(data);
-	    });
+		});
 	});
     });
 
     $("#join-game-btn").click(function() {
 	$("#root-home").load("waiting-room.html", function() {
-	    $.getScript("/script.js");
+	    //$.getScript("/script.js");
 	    $("#start-game").remove();
 	    $.getJSON( "/code-gen/", function( data ) {
 		$("#code").text(data);
@@ -20,8 +21,9 @@ $(function() {
 
     $(function() {
 	$("#back-btn").click(function() {
+	    //$.getScript("/script.js");
 	    $("#root-waiting-room").load("home.html");
-	    // $.getScript("/script.js");
 	});
     });
+
 });
