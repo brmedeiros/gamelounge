bb1cabe656723212f4ab3ae371ca817c0c1fd908
brmedeiros <br.medeiros@gmail.com>
2017-09-27 12:35:18 -0300

    remove unecessary comments from script.js

--- script.js
+++ script.js
@@ -41,42 +41,6 @@ function waitingRoomDefaultBehavior() {
     });
 }
 
-
-
-
 $(function() {
-
     loadHome();
-
-    // $("#create-new-game-btn").click(function() {
-    // 	$("#root").load("waiting-room-content.html", function() {
-    // 	    $("#join-waiting").remove();
-    // 	    $.getJSON( "/code-gen/", function( data ) {
-    // 		$("#code").text(data);
-    // 	    });
-    // 	});
-    // });
-
-    // $("#join-game-btn").click(function() {
-    // 	$("#root").load("waiting-room-content.html", function() {
-    // 	    $("#game-code").remove();
-    // 	    $("#start-game").remove();
-    // 	    $.getJSON( "/code-gen/", function( data ) {
-    // 		$("#code").text(data);
-    // 	    });
-    // 	});
-    // });
-
-    // $("#back-btn").click(function() {
-    // 	$("#root").load("home-content.html");
-    // });
-
-    // $("#p1-ready-btn").click(function() {
-    // 	$("#p1-ready").toggleClass("fa-question fa-check");
-    // });
-
-    // $("#p2-ready-btn").click(function() {
-    // 	$("#p2-ready").toggleClass("fa-question fa-check");
-    // });
-
-});
+ });
